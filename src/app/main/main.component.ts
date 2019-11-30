5c4f84088fe0c238381a55e89394912125f1321c
Mateusz Belka <mateusz.belka14@gmail.com>
2019-11-30 23:42:01 +0100

    main component functionality added

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.css"]
 })
 export class MainComponent implements OnInit {
+  thought = "";
+  thoughts: String[]  = [];
+  dates: String[] = [];
+  months = ["Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik","Listopad", "Grudzień"]
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {}
+  
+  ngOnInit() {}
+  
+  onThoughtCreate() {
+    this.thoughts.push(this.thought);
+    this.createDate();
   }
 
+  createDate(){
+    let date = new Date();
+    let datefixed = `${date.getDate()}. ${this.months[date.getMonth()]}  ${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    this.dates.push(datefixed)
+  }
+
+  checkEnter(event: KeyboardEvent){
+    if(event.key === "Enter"){
+      this.onThoughtCreate();
+    }
+  }
 }
